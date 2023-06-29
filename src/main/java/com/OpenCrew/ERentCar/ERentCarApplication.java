03dce6e86deb3e73c1a6fda53e7c52bea4bad586
SergioMG1259 <u20181h016@upc.edu.pe>
2023-06-29 16:20:20 -0500

    first commit

---
+++ src/main/java/com/OpenCrew/ERentCar/ERentCarApplication.java
@@ -0,0 +1,13 @@
+package com.OpenCrew.ERentCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERentCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERentCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OpenCrew/ERentCar/ERentCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OpenCrew.ERentCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERentCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
